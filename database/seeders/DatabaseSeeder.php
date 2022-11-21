13fda29c820e8825d6d25dd037e7c9e6b9cacf83
Yousef <yossfeabdaslm123@gmail.com>
2022-11-21 22:16:19 +0200

    services

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use App\Models\Services;
+use App\Models\SubCategory;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -21,11 +25,67 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        $this->call([
-            UserSeeder::class
-        ]);
+//        $this->call([
+//            UserSeeder::class
+//        ]);
+//        User::factory(3)->create();
+
         $this->call([
             CitiesSeeder::class
         ]);
+
+
+
+        //*******************************   category     *************************************************
+
+        Category::truncate();
+        $wedding=Category::create([
+            'name'=>'زفاف',
+        ]);
+        $graduate=Category::create([
+            'name'=>'تخرج',
+        ]);
+        $meeting=Category::create([
+            'name'=>'مؤتمرات',
+        ]);
+
+        //*******************************   sub category     *************************************************
+
+
+        SubCategory::truncate();
+        $camera=SubCategory::create([
+            'category_id' =>$wedding->id,
+            'name'=>'كاميرا',
+        ]);
+        SubCategory::create([
+            'category_id' =>$wedding->id,
+            'name'=>'فيديو',
+        ]);
+        SubCategory::create([
+            'category_id' =>$wedding->id,
+            'name'=>'اعمالنا فى الزفاف ',
+        ]);
+
+        //*******************************   services     *************************************************
+         Services::create([
+            'subcategory_id' =>$camera->id,
+            'name'=>'اسم الخدمه  ',
+            'description'=>' وصف الخدمه ',
+            'price'=>400,
+
+        ]);
+        Services::create([
+            'subcategory_id' =>$camera->id,
+            'name'=>'اسم الخدمه  ',
+            'description'=>' وصف الخدمه ',
+            'price'=>1000,
+
+        ]);    Services::create([
+        'subcategory_id' =>$camera->id,
+        'name'=>'اسم الخدمه  ',
+        'description'=>' وصف الخدمه ',
+        'price'=>400,
+    ]);
+
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CitiesController;
 use App\Http\Controllers\LoginUserController;
 use App\Http\Controllers\RegisterUserController;
+use App\Http\Controllers\ServicesController;
+use App\Http\Controllers\SubCategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,10 +23,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::apiResource('cities', CitiesController::class);
-//    ->middleware('auth:sanctum');
+Route::apiResource('user/cities', CitiesController::class)
+    ->middleware('auth:sanctum');
 Route::apiResource('user/register',RegisterUserController::class);
 //    Route::apiResource('user/login',LoginUserController::class);
 Route::post('/user/login', [LoginUserController::class, 'loginUser']);
+Route::apiResource('user/category',CategoryController::class);
+Route::apiResource('user/sub',SubCategoryController::class);
+Route::apiResource('user/services',ServicesController::class);
 
 
