e363104d9c63b84d7903222ee4087a81d7d52f30
Yousef <yossfeabdaslm123@gmail.com>
2023-02-14 18:18:27 +0200

    dashboard

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Admin;
+use App\Http\Requests\StoreAdminRequest;
+use App\Http\Requests\UpdateAdminRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+
+class AdminController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.signin');
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreAdminRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(){
+        //validation
+        $attributes=request()->validate([
+            'email' =>'required|email',
+            'password' =>'required'
+        ]);
+        //attempt to authentication
+//        if (!auth()->attempt($attributes)){
+//            throw ValidationException::withMessages(['email' =>'Your provided credential could not be verified']);
+//
+//        }
+
+        session()->regenerate();
+        //redirect with a flash message
+        return redirect('Dashboard')->with('success','Welcome Back');
+
+    }
+
+
+
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Admin  $admin
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Admin $admin)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Admin  $admin
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Admin $admin)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateAdminRequest  $request
+     * @param  \App\Models\Admin  $admin
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateAdminRequest $request, Admin $admin)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Admin  $admin
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Admin $admin)
+    {
+        //
+    }
+}
